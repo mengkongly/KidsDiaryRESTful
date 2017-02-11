17474739737058bddbc563c3792ba546af3b2d19
Nguonchhay <nguonchhay.touch@gmail.com>
2017-02-11 13:49:15 +0700

    [TASK] Udpate README and add database structure

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# Welcome to Kids Diary APP
+# Welcome to Kids Diary RESTful API
 
 Kids Diary App is a RESTful API that was built using Java Spring technologies. This is a project to finish
 Advanced Java course.
 
 # Maintainers
 
-	1. Touch Nguonchhay
-	2. Tes Putthira
-	3. Ly Mengkong
-	4. Kour Sotheareach
-	5. Mok Vanvannak
+- Touch Nguonchhay
+- Tes Putthira
+- Ly Mengkong
+- Kour Sotheareach
+- Mok Vanvannak
 
 # Development environment
 
@@ -19,9 +19,15 @@ Advanced Java course.
 
 # Local development
 
+- Clone project from github: `git clone https://github.com/Nguonchhay/KidsDiaryRESTful.git kids-diary`
+- Go to root directory of project: `cd kids-diary`
+- Create database name `kidsdiary` and import database structure from `kids-diary/src/main/resources/kidsdiary.sql`
+
+# Starting up server
+	
 	mvn clean jetty:run
 
-# API doc for requesting RESTful API
+# RESTful API Document
 
 
 
---
+++ src/main/resources/kidsdiary.sql
@@ -0,0 +1,177 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.10
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Feb 11, 2017 at 10:09 AM
+-- Server version: 5.5.38
+-- PHP Version: 5.6.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `kidsdiary`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activities`
+--
+
+CREATE TABLE `activities` (
+  `id` bigint(11) NOT NULL,
+  `name` varchar(35) COLLATE utf8_unicode_ci NOT NULL,
+  `icon` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `score` tinyint(2) NOT NULL DEFAULT '0',
+  `note` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `is_activated` tinyint(2) NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `daily_activities`
+--
+
+CREATE TABLE `daily_activities` (
+  `id` bigint(20) NOT NULL,
+  `parent` bigint(20) NOT NULL,
+  `child` bigint(20) NOT NULL,
+  `activity_date` datetime NOT NULL,
+  `is_approved` tinyint(2) NOT NULL DEFAULT '0',
+  `deleted_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `daily_activity_detail`
+--
+
+CREATE TABLE `daily_activity_detail` (
+`id` bigint(20) NOT NULL,
+  `daily_activity_id` bigint(20) NOT NULL,
+  `activity_id` bigint(20) NOT NULL,
+  `score` tinyint(2) NOT NULL DEFAULT '0',
+  `is_approved` tinyint(2) NOT NULL DEFAULT '0',
+  `note` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `deleted_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `families`
+--
+
+CREATE TABLE `families` (
+  `father` bigint(20) NOT NULL,
+  `mother` bigint(20) NOT NULL,
+  `child` bigint(20) NOT NULL,
+  `note` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL,
+  `username` varchar(35) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(35) COLLATE utf8_unicode_ci NOT NULL,
+  `access_token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `loggedin_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `email` varchar(35) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `first_name` varchar(35) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(35) COLLATE utf8_unicode_ci NOT NULL,
+  `sex` varchar(2) COLLATE utf8_unicode_ci NOT NULL,
+  `birth_date` timestamp NULL DEFAULT NULL,
+  `country` varchar(35) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `user_type` bigint(11) NOT NULL,
+  `is_activated` tinyint(2) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_types`
+--
+
+CREATE TABLE `user_types` (
+  `id` bigint(11) NOT NULL,
+  `type` varchar(35) COLLATE utf8_unicode_ci NOT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `user_types`
+--
+
+INSERT INTO `user_types` (`id`, `type`, `deleted_at`) VALUES
+(1, 'Wife', NULL),
+(2, 'Husband', NULL),
+(3, 'Child', NULL),
+(4, 'Adopter', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `activities`
+--
+ALTER TABLE `activities`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `daily_activities`
+--
+ALTER TABLE `daily_activities`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `daily_activity_detail`
+--
+ALTER TABLE `daily_activity_detail`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_types`
+--
+ALTER TABLE `user_types`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `daily_activity_detail`
+--
+ALTER TABLE `daily_activity_detail`
+MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
